571b5cf32f94d036b03ca0bcc8e52b6b5b63e7ec
Gabriel Ribas <seu@email.com>
2026-06-18 16:28:07 -0300

    refactor/themes-tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,17 +6,5 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-    // require('@tailwindcss/forms'),
-    // require('@tailwindcss/typography'),
-    // require('@tailwindcss/container-queries'),
   ]
 }
