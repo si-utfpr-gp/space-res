70e7078adf95c21c9ce1d5eee9f8bde82d73e673
Gabriel Ribas <seu@email.com>
2026-06-25 14:38:02 -0300

    feat/reservation-step-forms

--- app/controllers/users/reservations_controller.rb
+++ app/controllers/users/reservations_controller.rb
@@ -1,14 +1,38 @@
 class Users::ReservationsController < Users::BaseController
   def new
-    @reservation = Reservation.new
+    session[:reservation] ||= {}
+    @step = params[:step]&.to_i || 1
+    @spaces = Space.where(status: true)
   end
 
   def create
-    @reservation = Reservation.new(reservation_params)
-    if @reservation.save
-      redirect_to users_root_path, notice: t("reservations.created")
-    else
-      render :new, status: :unprocessable_entity
+    @step = params[:step].to_i
+    session[:reservation] ||= {}
+
+    case @step
+    when 1
+      session[:reservation]["space_id"] = params[:space_id]
+      redirect_to new_users_reservation_path(step: 2)
+
+    when 2
+      session[:reservation]["tipo_reserva"] = params[:tipo_reserva]
+      session[:reservation]["datas"] = params[:datas]
+      redirect_to new_users_reservation_path(step: 3)
+
+    when 3
+      @reservation = Reservation.new(
+        space_id:     session[:reservation]["space_id"],
+        tipo_reserva: session[:reservation]["tipo_reserva"],
+        status:       "draft",
+        user:         Current.user
+      )
+      
+      if @reservation.save
+        session.delete(:reservation)
+        redirect_to users_root_path, notice: t("reservations.created")
+      else
+        redirect_to new_users_reservation_path(step: 1)
+      end
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.find_or_create_by!(email_address: "admin@utfpr.edu.br") do |u|
+  u.name = "Admin"
+  u.password = "123456"
+  u.tipo_usuario = "admin"
+end
+
+User.find_or_create_by!(email_address: "aluno@utfpr.edu.br") do |u|
+  u.name = "Aluno1"
+  u.password = "123456"
+  u.tipo_usuario = "student"
+end
+
+sector = Sector.find_or_create_by!(nome: "TI") do |s|
+  s.responsavel_id = User.find_by(email_address: "admin@utfpr.edu.br").id
+end
+
+Space.find_or_create_by!(nome: "Sala 101") do |s|
+  s.tipo_espaco = "sala"
+  s.capacidade = 30
+  s.status = true
+  s.requer_aprovacao = false
+  s.sector = sector
+end
+
+Space.find_or_create_by!(nome: "Lab de Informática") do |s|
+  s.tipo_espaco = "laboratorio"
+  s.capacidade = 20
+  s.status = true
+  s.requer_aprovacao = true
+  s.sector = sector
+end
