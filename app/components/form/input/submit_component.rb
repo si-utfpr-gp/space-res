1ff540d6c7641d75d65047d74316067c9b00a9c0
Diego Marczal <marczal@utfpr.edu.br>
2026-06-30 20:23:19 -0300

    Review submit btn

--- app/components/form/input/submit_component.rb
+++ app/components/form/input/submit_component.rb
@@ -8,13 +8,38 @@ class Form::Input::SubmitComponent < ViewComponent::Base
   end
 
   def submit
-    icon_name = @options.delete(:icon)
-    icon_class = @options.delete(:icon_class) || "h-3.5 w-3.5"
-    classes = "btn-primary inline-flex items-center justify-center gap-2 rounded-md px-4 py-2 text-xs md:text-sm font-semibold cursor-pointer"
-    @options[:class] = "#{classes} #{@options[:class]}"
+    icon_options = @options.delete(:icon) || {}
+    loading_text = @options.delete(:loading_text) || I18n.t("actions.loading")
+
+    @options[:class] = class_names(
+      "btn btn-md btn-primary",
+      @options[:class]
+    )
+
+    @options[:data] = (@options[:data] || {}).merge(
+      turbo_submits_with: @options.dig(:data, :turbo_submits_with) || loading_text
+    )
 
     content_tag(:button, type: "submit", name: "commit", value: @value, **@options) do
-      safe_join([ @value, (helpers.icon(icon_name, class: icon_class) if icon_name) ].compact, " ")
+      safe_join(button_content(icon_options))
     end
+  end
+
+  private
+
+  def button_content(icon_options)
+    label = content_tag(:span, @value)
+    icon = build_icon(icon_options)
+
+    icon_options.fetch(:position, :start).to_sym == :end ? [label, icon].compact : [icon, label].compact
+  end
+
+  def build_icon(icon_options)
+    icon_name = icon_options[:name]
+    return unless icon_name
+
+    icon_class = icon_options.fetch(:class, "h-3.5 w-3.5")
+
+    helpers.icon(icon_name, class: icon_class)
   end
 end
